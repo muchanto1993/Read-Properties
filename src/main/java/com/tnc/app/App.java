b59f63cef78a67de34204f4e8fc2503025c62c68
Anto <m.antoberlianto1993@gmail.com>
2020-12-11 07:10:25 +0700

    first commit

---
+++ src/main/java/com/tnc/app/App.java
@@ -0,0 +1,14 @@
+package com.tnc.app;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) throws IOException {
+        CrunchifyGetPropertyValues properties = new CrunchifyGetPropertyValues();
+        properties.getPropValues();
+    }
+}
